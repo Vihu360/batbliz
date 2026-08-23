bc88000372b27e63cfde54ef1464f026865c65bc
Vivek Barnwal <vivekbarnwal360@gmail.com>
2026-08-23 12:36:32 +0530

    added cors fix

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://batbliz-admin.vercel.app", "http://localhost:3002", "http://localhost:3002/"],
+  origin: ["http://localhost:3000", "https://admin.cricvot.com", "https://batbliz-admin.vercel.app", "http://localhost:3002", "http://localhost:3002/"],
   credentials: true
 }));
 
